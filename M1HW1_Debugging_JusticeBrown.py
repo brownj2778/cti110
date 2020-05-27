f848aaa032b990bfd8adb2bf9907ac825bc5f2e7
brownj2778 <61815059+brownj2778@users.noreply.github.com>
2020-05-27 13:45:40 -0400

    Debugged strings into a For loop

---
+++ M1HW1_Debugging_JusticeBrown.py
@@ -0,0 +1,19 @@
+# Debugging provided code
+# 5/27/2020
+# CSC121 M1HW1– Debugging
+# Justice Brown
+
+#Set variable.
+x = 0
+
+#display the list for square and cube of the number
+print('number\t square\t cube')
+
+#For 5 repitions print the double and cube of x
+for count in range(6):
+    
+    print(x, '\t', x ** 2, '\t', x ** 3)
+    x = x + 1
+    
+
+
